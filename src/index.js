3f7297573edce02562dbe110d5d2b221eca995b3
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-10-29 18:42:47 -0600

    se agrego una variable para el puerto, se agrego la solucion del crossorigin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import { PORT } from '../config/config.js';
 
+//se implementa el modulo de express
 const app = express();
 
-app.listen(3300);
+app.use(express.json());
+
+//se resuelve el problema de crossorigin
+app.use((req,res, next)=>{
+    res.append('Access-Control-Allow-Origin',['*']);
+    res.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers','Content-type');
+    next();
+});
+
+// se pone a la escucha el servidor
+app.listen(PORT);
 
 
-console.log('Server runing on port 3300');
+console.log(`Server runing on port ${PORT}`);
 console.log('Happy Hakcing :)');
