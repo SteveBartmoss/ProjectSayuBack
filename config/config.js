68718692cc7e62138cbf10b65e21b06213adf918
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-10-29 18:46:10 -0600

    se agrego un archivo para los parametros de los servidores

---
+++ config/config.js
@@ -0,0 +1,16 @@
+//archivo para confogirar parametros
+
+export const HOST='localhost';
+
+export const DBPORT=3306;
+
+export const SOCKET='/var/run/mysqld/mysqld.sock';
+
+export const USER='tony';
+
+export const PASS='T0n1h4ll4n1472';
+
+export const DATABASE='tablero';
+
+export const PORT=3300;
+
