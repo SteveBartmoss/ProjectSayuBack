e792bad7373e7136160b9e4ffca3797dd1b17979
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-14 21:47:45 -0600

    se creo un archivo para los controllers de las notas, se creo una funcion para crear una nota

---
+++ controllers/notasControllers.js
@@ -0,0 +1,11 @@
+import { conexion } from "../config/conexion.js";
+
+export const getNotas=async(req,res)=>{
+    const idusuario=req.params.id
+    try{
+        const [result] = await conexion.query('SELECT * FROM notas WHERE autor=?',[idusuario]);
+        res.json(result);
+    }catch(error){
+        return res.status(500).json({message: error.message});
+    }
+}
