abe649e32f3cb2e9ac027ed5462f3b6319af0c2d
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-20 00:50:40 -0600

    se agrego una funcion para actualizar una nota

--- controllers/notasControllers.js
+++ controllers/notasControllers.js
@@ -24,6 +24,15 @@ export const createNota=async(req,res)=>{
     }
 }
 
+export const updateNota=async(req,res)=>{
+    const {titulo,descripcion}=req.body;
+    try{
+
+    }catch(error){
+        return res.status(500).json({message: error.message});
+    }
+}
+
 export const deleteNota=async(req,res)=>{
     const id=req.params.id;
     try{
