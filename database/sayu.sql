38ed8331289277b17eb37769b0db383343f520ea
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-12-03 14:18:11 -0600

    Se agrego la tabla de los perfiles de usuario

--- database/sayu.sql
+++ database/sayu.sql
@@ -1,9 +1,24 @@
 create database projectSayu;
 
-create table users{
+create table users(
     id int auto_increment primary key,
     userName varchar(30) not null,
     password varchar(120) not null,
-}
+);
+
+create table userProfile(
+    id int auto_increment primary key,
+    idUser int,
+    userName varchar(60),
+    firstLastName varchar(60),
+    secondLastName varchar(60),
+    email varchar(60),
+    cellphone varchar(15),
+
+    constraint userProfileIdUser
+        foreign key (idUser) references users (id)
+        on delete set null
+        on update cascade
+);
 
 
